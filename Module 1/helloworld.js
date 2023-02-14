d66de334f74aa6f5d02039dc8fe269df33a16fa5
Chris <christopherm4rsh@gmail.com>
2023-02-14 16:57:46 +1100

    final commit for 14/02 coursework IOD SE

--- Module 1/helloworld.js
+++ Module 1/helloworld.js
@@ -14,27 +14,26 @@ function multiplication (a, b){
     return a * b;
 }
 
+function modulo(a, b){
+    return a%b;
+}
 function userGreeting(a){
     console.log(`Hello ${a}`);
 }
 
 userGreeting("Chris");
 
-console.log(subtraction(3, 2));
-console.log(addition(3, 2));
-console.log(division(4, 2));
-console.log(multiplication(3, 2));
+// console.log(subtraction(3, 2));
+// console.log(addition(3, 2));
+// console.log(division(4, 2));
+// console.log(multiplication(3, 2));
+// console.log(modulo(10,4));
+
 
 var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+var result = sentiment.analyze('Cats are stupid.');
+console.dir(result); // Score: -2, Comparative: -0.666
 
-
-function analyseSentence(sentence){
-    var sentiment = new Sentiment();
-    var result = sentiment.analyze(sentence);
-    return result.score;
-}
-
-analyseSentence("i love chocolate but i hate that it makes me fat")
-
-console.log(`Your score is ${result.score}`);
-
+let text2 = sentiment.analyze("It’s sad people see someone at their lowest as an opportunity to take a video to make fun of them rather than show compassion or kindness. Shame on anyone who supports this.")
+console.dir(text2);
